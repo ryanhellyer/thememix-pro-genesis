08216f5b5394e47b41a103316bcf3ffd3c7abbea
Ryan Hellyer <ryanhellyer@gmail.com>
2016-02-10 16:19:21 +0100

    Implementing size and positioning settings on front end #3

--- modules/featured-content-widget/font-awesome/font-awesome.php
+++ modules/featured-content-widget/font-awesome/font-awesome.php
@@ -35,14 +35,37 @@ function themefix_font_awesome_settings_extension( $args ) {
 				true
 			),
 		),
+
+		'fontawesome-size' => array(
+			'label'       => __( 'Size', 'thememixfc' ),
+			'description' => '',
+			'type'        => 'select',
+			'options'     => array(
+				'lg' => __( 'Normal', 'thememix-pro-genesis' ),
+				'1x' => '1x',
+				'2x' => '2x',
+				'3x' => '3x',
+				'4x' => '4x',
+				'5x' => '5x',
+				'6x' => '6x',
+			),
+			'requires'    => array(
+				'font-awesome',
+				'',
+				true
+			),
+		),
+
+
 		'fontawesome-position' => array(
 			'label'       => __( 'Position', 'thememixfc' ),
 			'description' => '',
 			'type'        => 'select',
 			'options'     => array(
-				'top'    => __( 'Top', 'thememix-pro-genesis' ),
-				'middle' => __( 'Middle', 'thememix-pro-genesis' ),
-				'bottom' => __( 'Bottom', 'thememix-pro-genesis' ),
+				'before_title'        => __( 'Before title (centered)', 'thememix-pro-genesis' ),
+				'inline_before_title' => __( 'Inline before title', 'thememix-pro-genesis' ),
+				'inline_after_title'  => __( 'Inline after title', 'thememix-pro-genesis' ),
+				'after_title'         => __( 'After title (centered)', 'thememix-pro-genesis' ),
 			),
 			'requires'    => array(
 				'font-awesome',
@@ -76,30 +99,58 @@ add_action( 'thememixfc_before_post_content', 'thememixfc_top_fontawesome' );
 function themefix_fontawesome_init() {
 	$settings = get_option( 'widget_featured-content' );
 	if ( isset( $settings[3]['font-awesome'] ) && 1 == $settings[3]['font-awesome'] && isset( $settings[3]['fontawesome-position'] ) ) {
+		$position = $settings[3]['fontawesome-position'];
 
-		if ( 'middle' == $settings[3]['fontawesome-position'] ) {
-			$position = 'before_title';
+		$positions = array(
+			'inline_before_title',
+			'inline_after_title',
+		);
+		if ( in_array( $position, $positions ) ) {
+			add_filter( 'thememixfc_post_title_pattern', 'thememixfc_' . $position . '_fontawesome' );
 		}
 
-		// If position set, then add filter
-		if ( isset( $position ) ) {
-			add_filter( 'thememixfc_post_title_pattern', 'thememixfc_' . $position . '_fontawesome' );
+		if ( 'before_title' == $position ) {
+			add_action( 'thememixfc_before_post_content', 'thememixfc_' . $position . '_fontawesome', 1 );
+		}
+
+		if ( 'after_title' == $position ) {
+			add_action( 'thememixfc_after_post_content', 'thememixfc_' . $position . '_fontawesome' );
 		}
 
 	}
 
-
 }
 add_action( 'init', 'themefix_fontawesome_init' );
 
-/**
- *
- */
-function thememixfc_before_title_fontawesome( $content ) {
-	$content = str_replace( '%s%s%s', '%s<span class="fa fa-camera-retro fa-3x"></span>%sA%s', $content );
+function thememixfc_before_title_fontawesome() {
+	echo '<div style="width:100%;text-align:center;"><span class="fa fa-camera-retro fa-' . thememixfc_get_size_fontawesome() . '"></span></div>';
+}
+
+function thememixfc_after_title_fontawesome() {
+	echo '<div style="width:100%;text-align:center;"><span class="fa fa-camera-retro fa-' . thememixfc_get_size_fontawesome() . '"></span></div>';
+}
+
+function thememixfc_get_size_fontawesome() {
+
+	$settings = get_option( 'widget_featured-content' );
+	if ( isset( $settings[3]['fontawesome-size'] ) ) {
+		$size = $settings[3]['fontawesome-size'];
+	} else {
+		$size = 'lg';
+	}
+
+	return $size;
+}
+
+function thememixfc_inline_before_title_fontawesome( $content ) {
+	$content = str_replace( '%s%s%s', '%s<span class="fa fa-camera-retro fa-' . thememixfc_get_size_fontawesome() . '"></span>%s%s', $content );
 	return $content;
 }
 
+function thememixfc_inline_after_title_fontawesome( $content ) {
+	$content = str_replace( '%s%s%s', '%s%s<span class="fa fa-camera-retro fa-' . thememixfc_get_size_fontawesome() . '"></span>%s', $content );
+	return $content;
+}
 
 /**
  * Add Font Awesome stylesheet.
