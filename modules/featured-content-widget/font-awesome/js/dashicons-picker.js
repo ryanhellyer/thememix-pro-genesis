f7627d0bf3ec146f1f16f68ba671091c35e3789d
Ryan Hellyer <ryanhellyer@gmail.com>
2016-02-18 13:49:33 +0100

    Relabelling some dashicons items #3

--- modules/featured-content-widget/font-awesome/js/dashicons-picker.js
+++ modules/featured-content-widget/font-awesome/js/dashicons-picker.js
@@ -10,7 +10,7 @@
 	 *
 	 * @returns {void}
 	 */
-	$.fn.dashiconsPicker = function () {
+	$.fn.fontawesomePicker = function () {
 
 		/**
 		 * Dashicons, in CSS order
@@ -623,271 +623,28 @@
 			'bluetooth',
 			'bluetooth-b',
 			'percent',
-
-
-
-			'menu',
-			'admin-site',
-			'dashboard',
-			'admin-media',
-			'admin-page',
-			'admin-comments',
-			'admin-appearance',
-			'admin-plugins',
-			'admin-users',
-			'admin-tools',
-			'admin-settings',
-			'admin-network',
-			'admin-generic',
-			'admin-home',
-			'admin-collapse',
-			'filter',
-			'admin-customizer',
-			'admin-multisite',
-			'admin-links',
-			'format-links',
-			'admin-post',
-			'format-standard',
-			'format-image',
-			'format-gallery',
-			'format-audio',
-			'format-video',
-			'format-chat',
-			'format-status',
-			'format-aside',
-			'format-quote',
-			'welcome-write-blog',
-			'welcome-edit-page',
-			'welcome-add-page',
-			'welcome-view-site',
-			'welcome-widgets-menus',
-			'welcome-comments',
-			'welcome-learn-more',
-			'image-crop',
-			'image-rotate',
-			'image-rotate-left',
-			'image-rotate-right',
-			'image-flip-vertical',
-			'image-flip-horizontal',
-			'image-filter',
-			'undo',
-			'redo',
-			'editor-bold',
-			'editor-italic',
-			'editor-ul',
-			'editor-ol',
-			'editor-quote',
-			'editor-alignleft',
-			'editor-aligncenter',
-			'editor-alignright',
-			'editor-insertmore',
-			'editor-spellcheck',
-			'editor-distractionfree',
-			'editor-expand',
-			'editor-contract',
-			'editor-kitchensink',
-			'editor-underline',
-			'editor-justify',
-			'editor-textcolor',
-			'editor-paste-word',
-			'editor-paste-text',
-			'editor-removeformatting',
-			'editor-video',
-			'editor-customchar',
-			'editor-outdent',
-			'editor-indent',
-			'editor-help',
-			'editor-strikethrough',
-			'editor-unlink',
-			'editor-rtl',
-			'editor-break',
-			'editor-code',
-			'editor-paragraph',
-			'editor-table',
-			'align-left',
-			'align-right',
-			'align-center',
-			'align-none',
-			'lock',
-			'unlock',
-			'calendar',
-			'calendar-alt',
-			'visibility',
-			'hidden',
-			'post-status',
-			'edit',
-			'post-trash',
-			'trash',
-			'sticky',
-			'external',
-			'arrow-up',
-			'arrow-down',
-			'arrow-left',
-			'arrow-right',
-			'arrow-up-alt',
-			'arrow-down-alt',
-			'arrow-left-alt',
-			'arrow-right-alt',
-			'arrow-up-alt2',
-			'arrow-down-alt2',
-			'arrow-left-alt2',
-			'arrow-right-alt2',
-			'leftright',
-			'sort',
-			'randomize',
-			'list-view',
-			'excerpt-view',
-			'grid-view',
-			'hammer',
-			'art',
-			'migrate',
-			'performance',
-			'universal-access',
-			'universal-access-alt',
-			'tickets',
-			'nametag',
-			'clipboard',
-			'heart',
-			'megaphone',
-			'schedule',
-			'wordpress',
-			'wordpress-alt',
-			'pressthis',
-			'update',
-			'screenoptions',
-			'cart',
-			'feedback',
-			'cloud',
-			'translation',
-			'tag',
-			'category',
-			'archive',
-			'tagcloud',
-			'text',
-			'media-archive',
-			'media-audio',
-			'media-code',
-			'media-default',
-			'media-document',
-			'media-interactive',
-			'media-spreadsheet',
-			'media-text',
-			'media-video',
-			'playlist-audio',
-			'playlist-video',
-			'controls-play',
-			'controls-pause',
-			'controls-forward',
-			'controls-skipforward',
-			'controls-back',
-			'controls-skipback',
-			'controls-repeat',
-			'controls-volumeon',
-			'controls-volumeoff',
-			'yes',
-			'no',
-			'no-alt',
-			'plus',
-			'plus-alt',
-			'plus-alt2',
-			'minus',
-			'dismiss',
-			'marker',
-			'star-filled',
-			'star-half',
-			'star-empty',
-			'flag',
-			'info',
-			'warning',
-			'share',
-			'share1',
-			'share-alt',
-			'share-alt2',
-			'twitter',
-			'rss',
-			'email',
-			'email-alt',
-			'facebook',
-			'facebook-alt',
-			'networking',
-			'googleplus',
-			'location',
-			'location-alt',
-			'camera',
-			'images-alt',
-			'images-alt2',
-			'video-alt',
-			'video-alt2',
-			'video-alt3',
-			'vault',
-			'shield',
-			'shield-alt',
-			'sos',
-			'search',
-			'slides',
-			'analytics',
-			'chart-pie',
-			'chart-bar',
-			'chart-line',
-			'chart-area',
-			'groups',
-			'businessman',
-			'id',
-			'id-alt',
-			'products',
-			'awards',
-			'forms',
-			'testimonial',
-			'portfolio',
-			'book',
-			'book-alt',
-			'download',
-			'upload',
-			'backup',
-			'clock',
-			'lightbulb',
-			'microphone',
-			'desktop',
-			'tablet',
-			'smartphone',
-			'phone',
-			'smiley',
-			'index-card',
-			'carrot',
-			'building',
-			'store',
-			'album',
-			'palmtree',
-			'tickets-alt',
-			'money',
-			'thumbs-up',
-			'thumbs-down',
-			'layout',
-			'',
-			'',
-			''
 		];
 
 		return this.each( function () {
 
 			var button = $( this );
 
-			button.on( 'click.dashiconsPicker', function () {
+			button.on( 'click.fontawesomePicker', function () {
 				createPopup( button );
 			} );
 
 			function createPopup( button ) {
 
 				var target = $( button.data( 'target' ) ),
-					popup  = $( '<div class="dashicon-picker-container"> \
-						<div class="dashicon-picker-control" /> \
-						<ul class="dashicon-picker-list" /> \
+					popup  = $( '<div class="fontawesome-picker-container"> \
+						<div class="fontawesome-picker-control" /> \
+						<ul class="fontawesome-picker-list" /> \
 					</div>' )
 						.css( {
 							'top':  button.offset().top,
 							'left': button.offset().left
 						} ),
-					list = popup.find( '.dashicon-picker-list' );
+					list = popup.find( '.fontawesome-picker-list' );
 
 				for ( var i in icons ) {
 					list.append( '<li data-icon="' + icons[i] + '"><a href="#" title="' + icons[i] + '"><span class="icon fa-' + icons[i] + '"></span></a></li>' );
@@ -907,7 +664,7 @@
 					removePopup();
 				} );
 
-				var control = popup.find( '.dashicon-picker-control' );
+				var control = popup.find( '.fontawesome-picker-control' );
 
 				control.html( '<a data-direction="back" href="#"> \
 					<span class="dashicons dashicons-arrow-left-alt2"></span></a> \
@@ -949,14 +706,14 @@
 			}
 
 			function removePopup() {
-				$( '.dashicon-picker-container' ).remove();
+				$( '.fontawesome-picker-container' ).remove();
 				$( document ).unbind( '.dashicons-picker' );
 			}
 		} );
 	};
 
 	$( function () {
-		$( '.dashicons-picker' ).dashiconsPicker();
+		$( '.dashicons-picker' ).fontawesomePicker();
 	} );
 
 }( jQuery ) );
